9606ba921b330630814e046c31ed7581b1511f07
ytyht226 <ytyht226@163.com>
2018-12-14 19:11:38 +0800

    spring-annotation

--- spring-annotation/src/test/java/com/jd58/test/IocTestAop.java
+++ spring-annotation/src/test/java/com/jd58/test/IocTestAop.java
@@ -3,6 +3,7 @@ package com.jd58.test;
 import com.jd58.aop.MathCalculator;
 import com.jd58.config.MainConfigOfAop;
 import org.junit.Test;
+import org.springframework.cglib.core.DebuggingClassWriter;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -12,6 +13,8 @@ public class IocTestAop {
 
     @Test
     public void test01() {
+//        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "D:\\class");
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(MainConfigOfAop.class);
         MathCalculator mathCalculator = ac.getBean(MathCalculator.class);
         mathCalculator.div(5, 5);
